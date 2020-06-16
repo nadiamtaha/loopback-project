be93696782003ea73d5dbf7bbcee1a54a11882d9
nadia taha <nadia.mohamed.taha166@gmail.com>
2020-06-16 04:41:32 +0200

    first commit

---
+++ phonebook/src/models/contact.model.ts
@@ -0,0 +1,42 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Contact extends Entity {
+  static validatesUniquenessOf(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phonenumber: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Contact>) {
+    super(data);
+  }
+}
+
+export interface ContactRelations {
+  // describe navigational properties here
+}
+
+export type ContactWithRelations = Contact & ContactRelations;
